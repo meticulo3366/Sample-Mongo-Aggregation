dda21603419a1313c58a2a15431a7493dd42e2d0
Ubuntu <ubuntu@ip-172-31-30-245.ec2.internal>
2015-01-06 06:34:25 +0000

    cleaned up project structure

--- getAllLicenses.sh
+++ getAllLicenses.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-mongo localhost:27017/agency-node-nsf          getLicense.js   > agency-node-nsf.json
-mongo localhost:27017/agency-node-usdoa        getLicense.js > agency-node-usdoa.json
-mongo localhost:27017/agency-node-usaid        getLicense.js > agency-node-usaid.json
-mongo localhost:27017/agency-node-nasa         getLicense.js > agency-node-nasa.json
-mongo localhost:27017/agency-node-usdhhs       getLicense.js > agency-node-usdhhs.json
-mongo localhost:27017/agency-node-usdod        getLicense.js > agency-node-usdod.json
-mongo localhost:27017/agency-node-nist         getLicense.js > agency-node-nist.json
-mongo localhost:27017/agency-node-usdoe        getLicense.js > agency-node-usdoe.json
-mongo localhost:27017/agency-node-smithsonian  getLicense.js > agency-node-smithsonian.json
-mongo localhost:27017/agency-node-usdoi        getLicense.js > agency-node-usdoi.json
+# execute the javascript for each agency and print out the results
+mongo localhost:27017/agency-node-nsf          getLicense.js > licenseOutputs/agency-node-nsf.json
+mongo localhost:27017/agency-node-usdoa        getLicense.js > licenseOutputs/agency-node-usdoa.json
+mongo localhost:27017/agency-node-usaid        getLicense.js > licenseOutputs/agency-node-usaid.json
+mongo localhost:27017/agency-node-nasa         getLicense.js > licenseOutputs/agency-node-nasa.json
+mongo localhost:27017/agency-node-usdhhs       getLicense.js > licenseOutputs/agency-node-usdhhs.json
+mongo localhost:27017/agency-node-usdod        getLicense.js > licenseOutputs/agency-node-usdod.json
+mongo localhost:27017/agency-node-nist         getLicense.js > licenseOutputs/agency-node-nist.json
+mongo localhost:27017/agency-node-usdoe        getLicense.js > licenseOutputs/agency-node-usdoe.json
+mongo localhost:27017/agency-node-smithsonian  getLicense.js > licenseOutputs/agency-node-smithsonian.json
+mongo localhost:27017/agency-node-usdoi        getLicense.js > licenseOutputs/agency-node-usdoi.json
